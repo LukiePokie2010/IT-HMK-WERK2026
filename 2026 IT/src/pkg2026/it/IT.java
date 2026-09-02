61d361ecd578ab9e023375427f2aefdf035e7ad6
Luke.VanderMerwe <Luke.VanderMerwe@Lab1-Nuc4.helpmekaar.local>
2026-09-02 11:01:09 +0200

    Uploaded

---
+++ 2026 IT/src/pkg2026/it/IT.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg2026.it;
+
+/**
+ *
+ * @author Luke.VanderMerwe
+ */
+public class IT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
